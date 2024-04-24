b4213c65e987e8ee6521b12d54bf55c9cd980da7
Shakeelurrahman <114961796+Shakeelurrahman@users.noreply.github.com>
2024-04-24 13:29:44 +0500

    add files

---
+++ Penguin_Data/ZZfeatures map, EfficientSU2, 3 features/penguins_data_VQC_ZZfeaturemap with_reps=2_and EfficientSU2_with_reps=1, features=3.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report
+from scipy.stats import pearsonr
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+
+
+# In[2]:
+
+
+data = pd.read_csv('https://raw.githubusercontent.com/michellechandraa/michellechandraa/main/penguins.csv' , delimiter = ';')
+penguin_df = pd.DataFrame(data)
+penguin_df.head(5)
+
+
+# In[3]:
+
+
+##checking for missing values
+penguin_df.isnull().sum()
+
+
+# In[4]:
+
+
+#dropping missing values
+penguin_df.dropna(axis=0, inplace=True)
+penguin_df.isnull().sum()
+
+
+# In[5]:
+
+
+#check for duplicate values
+penguin_df.duplicated().value_counts()
+
+
+# In[6]:
+
+
+#label encoding
+labelencoder = preprocessing.LabelEncoder()
+penguin_df.sex = labelencoder.fit_transform(penguin_df.sex)
+penguin_df.island = labelencoder.fit_transform(penguin_df.island)
+penguin_df.species = labelencoder.fit_transform(penguin_df.species)
+penguin_df.head(5)
+
+
+# In[7]:
+
+
+#e=Exploratory Data Analysis
+penguin_df.info()
+
+
+# In[8]:
+
+
+#Univariable tendency and variation
+penguin_df.describe()
+
+
+# In[9]:
+
+
+#Data visualization
+cp = sns.countplot(data=penguin_df, x='island',hue='species',palette='viridis')
+cp.set_xticks([0, 1, 2])
+cp.set_xticklabels(['Torgersen', 'Biscoe','Dream'])
+plt.legend(labels=['Adelie' , 'Chinstrap' , 'Gentoo'])
+plt.title('The number of each species on each island')
+plt.xlabel('Island')
+plt.ylabel('amount')
+sns.despine()
+
+
+# In[10]:
+
+
+#check for correlation 
+corr_coef = penguin_df.corr()
+sns.heatmap(corr_coef, annot = True, cmap = 'coolwarm', fmt = '0.1f' , vmin = -1 , vmax = 1)
+
+
+# In[11]:
+
+
+#remove weak variables
+remove = []
+for data in penguin_df:
+    if data != 'species':
+        cor_coef, pvalue= pearsonr(penguin_df[data], penguin_df['species'])
+        if (cor_coef<0.5 and cor_coef>-0.5):                                                                              
+            remove.append(data)
+penguin_df.drop(remove, axis = 1, inplace = True)
+print(f'dataframe which has been subtracted {penguin_df.shape[1]} from the remaining coloumns.')
+
+
+# In[12]:
+
+
+#ordering the dataset columns
+penguin_df = penguin_df[['island', 'flipper_length_mm', 'body_mass_g', 'species']]
+penguin_df.head(5)
+
+
+# In[13]:
+
+
+#selecting the dependent and independent features
+features = penguin_df.iloc[:, :-1].values
+labels = penguin_df.iloc[:, -1].values
+
+
+# In[14]:
+
+
+#train test split
+from sklearn.preprocessing import MinMaxScaler
+
+features = MinMaxScaler().fit_transform(features)
+
+
+# In[15]:
+
+
+from sklearn.model_selection import train_test_split
+from qiskit.utils import algorithm_globals
+
+algorithm_globals.random_seed = 123
+train_features, test_features, train_labels, test_labels = train_test_split(
+    features, labels, train_size=0.8, random_state=algorithm_globals.random_seed
+)
+
+
+# In[16]:
+
+
+#Data encoder ZZfeature 
+from qiskit.circuit.library import ZZFeatureMap
+
+num_features = features.shape[1]
+
+feature_map = ZZFeatureMap(feature_dimension=num_features, reps=2)
+feature_map.decompose().draw(output="mpl", fold=20)
+
+
+# In[17]:
+
+
+#ansatz EfficientSU2
+from qiskit.circuit.library import EfficientSU2 
+
+ansatz = EfficientSU2(num_qubits=num_features, su2_gates=['rx', 'y'], entanglement='circular', reps=1)
+ansatz.decompose().draw(output="mpl", fold=20)
+
+
+# In[18]:
+
+
+#Optimizer
+from qiskit.algorithms.optimizers import COBYLA
+
+optimizer = COBYLA(maxiter=100)
+
+
+# In[19]:
+
+
+#training
+from qiskit.primitives import Sampler
+
+sampler = Sampler()
+
+
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+
+objective_func_vals = []
+plt.rcParams["figure.figsize"] = (12, 6)
+
+
+def callback_graph(weights, obj_func_eval):
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+    
+    
+    
+    
+import time
+from qiskit_machine_learning.algorithms.classifiers import VQC
+
+vqc = VQC(
+    sampler=sampler,
+    feature_map=feature_map,
+    ansatz=ansatz,
+    optimizer=optimizer,
+    callback=callback_graph,
+)
+
+# clear objective value history
+objective_func_vals = []
+
+start = time.time()
+vqc.fit(train_features, train_labels)
+elapsed = time.time() - start
+
+print(f"Training time: {round(elapsed)} seconds")
+
+
+# In[20]:
+
+
+train_score_q4 = vqc.score(train_features, train_labels)
+test_score_q4 = vqc.score(test_features, test_labels)
+
+print(f"Quantum VQC on the training dataset: {train_score_q4:.2f}")
+print(f"Quantum VQC on the test dataset:     {test_score_q4:.2f}")
+
+
+# In[ ]:
+
+
+
+
---
+++ Titanic_Data/ZZfeatures map, ReaAmplitude, 5 features, Cobyla/Titanic_data_VQC_ZZfeaturemap with_reps=3_and Real_amplitude_with_reps=1, features=5, Optimizer=Cobyla.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report
+from scipy.stats import pearsonr
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+from sklearn.datasets import load_diabetes
+
+
+# In[2]:
+
+
+#Loading Data
+titanic_data = pd.read_csv('https://raw.githubusercontent.com/amberkakkar01/Titanic-Survival-Prediction/master/train.csv')
+
+
+# In[3]:
+
+
+#printing data
+titanic_data.head()
+
+
+# In[4]:
+
+
+#looking for null values
+titanic_data.isnull().sum()
+
+
+# In[5]:
+
+
+#Dropping  the “Cabin” column from the data frame as it won’t be of much importance due to a lot of null values
+
+titanic_data = titanic_data.drop(columns='Cabin', axis=1)
+
+
+# In[6]:
+
+
+#Replacing the missing values in the “Age” column with the mean value
+
+titanic_data['Age'].fillna(titanic_data['Age'].mean(), inplace=True)
+
+
+# In[7]:
+
+
+#Finding the mode value of the “Embarked” column as it will have occurred the maximum number of times
+print(titanic_data['Embarked'].mode())
+
+
+# In[8]:
+
+
+#Replacing the missing values in the “Embarked” column with mode value
+titanic_data['Embarked'].fillna(titanic_data['Embarked'].mode()[0], inplace=True)
+
+
+# In[9]:
+
+
+#Checking for missing values again
+titanic_data.isnull().sum()
+
+
+# In[10]:
+
+
+#Converting the string values in 'Sex' and 'Embarked' columns into integer type values, and transform it into a categorical column:
+
+titanic_data.replace({'Sex':{'male':0,'female':1}, 'Embarked':{'S':0,'C':1,'Q':2}}, inplace=True)
+
+
+# In[11]:
+
+
+# Columns which are not of much importance in the  process maybe dropped.
+
+titanic_data= titanic_data.drop(columns = ['PassengerId','Name','Ticket'],axis=1)
+
+
+# In[12]:
+
+
+#Printing the processed data
+titanic_data.head()
+
+
+# In[13]:
+
+
+#Exploratory Data Analysis
+titanic_data.info()
+
+
+# In[14]:
+
+
+#Univariable tendency and variation
+titanic_data.describe()
+
+
+# In[15]:
+
+
+#Data visualization
+plt.figure(figsize=(10, 8))
+sns.heatmap(titanic_data.corr(), annot=True, cmap='coolwarm')
+plt.show()
+
+
+# In[16]:
+
+
+#remove weak variables
+remove = []
+for data in titanic_data:
+    if data != 'Survived':
+        cor_coef, pvalue= pearsonr(titanic_data[data], titanic_data['Survived'])
+        if (cor_coef<0.08 and cor_coef>-0.08):                                                                              
+            remove.append(data)
+titanic_data.drop(remove, axis = 1, inplace = True)
+print(f'dataframe which has been subtracted {titanic_data.shape[1]} from the remaining coloumns.')
+titanic_data.head(5)
+
+
+# In[17]:
+
+
+#ordering the dataset columns
+titanic_data = titanic_data[['Pclass', 'Sex' , 'Parch', 'Fare' , 'Embarked' , 'Survived']]
+titanic_data.head(5)
+
+
+# In[18]:
+
+
+#selecting dependent and independent features
+features = titanic_data.iloc[:, :-1].values
+labels = titanic_data.iloc[:, -1].values
+
+
+# In[19]:
+
+
+#Data Normalization
+from sklearn.preprocessing import MinMaxScaler
+
+features = MinMaxScaler().fit_transform(features)
+
+
+# In[20]:
+
+
+#Test, Train split
+from sklearn.model_selection import train_test_split
+from qiskit_algorithms.utils import algorithm_globals
+
+algorithm_globals.random_seed = 123
+train_features, test_features, train_labels, test_labels = train_test_split(
+    features, labels, train_size=0.8, random_state=algorithm_globals.random_seed
+)
+
+
+# In[21]:
+
+
+#Data encoder ZZfeature 
+from qiskit.circuit.library import ZZFeatureMap
+
+num_features = features.shape[1]
+
+feature_map = ZZFeatureMap(feature_dimension=num_features, reps=3)
+feature_map.decompose().draw(output="mpl", fold=20)
+
+
+# In[22]:
+
+
+#ansatz ReaAmplitudes
+from qiskit.circuit.library import RealAmplitudes
+
+ansatz = RealAmplitudes(num_qubits=num_features, reps=1)
+ansatz.decompose().draw(output="mpl", fold=20)
+
+
+# In[23]:
+
+
+#Optimizer
+from qiskit_algorithms.optimizers import COBYLA
+
+optimizer = COBYLA(maxiter=100)
+
+
+# In[24]:
+
+
+#Training
+from qiskit.primitives import Sampler
+
+sampler = Sampler()
+
+
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+
+objective_func_vals = []
+plt.rcParams["figure.figsize"] = (12, 6)
+
+
+def callback_graph(weights, obj_func_eval):
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+    
+    
+    
+    
+import time
+from qiskit_machine_learning.algorithms.classifiers import VQC
+
+vqc = VQC(
+    sampler=sampler,
+    feature_map=feature_map,
+    ansatz=ansatz,
+    optimizer=optimizer,
+    callback=callback_graph,
+)
+
+# Clear objective value history
+objective_func_vals = []
+
+start = time.time()
+vqc.fit(train_features, train_labels)
+elapsed = time.time() - start
+
+print(f"Training time: {round(elapsed)} seconds")
+
+
+# In[25]:
+
+
+train_score_q4 = vqc.score(train_features, train_labels)
+test_score_q4 = vqc.score(test_features, test_labels)
+
+print(f"Quantum VQC on the training dataset: {train_score_q4:.2f}")
+print(f"Quantum VQC on the test dataset:     {test_score_q4:.2f}")
+
+
+# In[ ]:
+
+
+
+
